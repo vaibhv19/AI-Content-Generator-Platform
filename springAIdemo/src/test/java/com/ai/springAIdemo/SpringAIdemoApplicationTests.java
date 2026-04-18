7e727b5adf43730d3f8353d1b953db1135cb6c2c
Vaibhav Gupta <vaibhavkrgupta369@gmail.com>
2026-04-18 10:33:20 +0530

    project setup done

---
+++ springAIdemo/src/test/java/com/ai/springAIdemo/SpringAIdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.springAIdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAIdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
