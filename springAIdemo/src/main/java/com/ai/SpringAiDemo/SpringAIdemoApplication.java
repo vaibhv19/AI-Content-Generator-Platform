7ea17fa509bade90648c898688bafeaab02babcd
Vaibhav Gupta <vaibhavkrgupta369@gmail.com>
2026-05-22 23:31:42 +0530

    Added GenAIController

---
+++ springAIdemo/src/main/java/com/ai/SpringAiDemo/SpringAIdemoApplication.java
@@ -0,0 +1,13 @@
+package com.ai.SpringAiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAIdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAIdemoApplication.class, args);
+	}
+
+}
